8c8cf60f0110d5a01a5cdfa9af1ac0a4be234e42
Adam Patrick <98496625+adam-patrick@users.noreply.github.com>
2023-04-02 20:07:28 -0500

    add Mod the Cube assignment

---
+++ Create with Code/Mod the Cube/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public GameObject currentGameObject;
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 2, 1);
+        transform.localScale = Vector3.one * 2.0f;
+        
+        Material material = Renderer.material;
+        material.color = new Color (10, 1, 5, 1);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
